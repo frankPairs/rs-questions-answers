3b6de347a53b388d29b91a7d59a8129858f760c4
Frank Parejo <francisco.parejo.lopez@gmail.com>
2022-08-29 23:24:15 +0200

    add on delete cascade to correspoding question field in answers table.

--- migrations/20220716094815_answers.up.sql
+++ migrations/20220716094815_answers.up.sql
@@ -3,5 +3,5 @@ CREATE TABLE IF NOT EXISTS answers (
     id serial PRIMARY KEY,
     content TEXT NOT NULL,
     created_on TIMESTAMP NOT NULL DEFAULT NOW(),
-    corresponding_question integer REFERENCES questions
+    corresponding_question integer REFERENCES questions ON DELETE CASCADE
 );
