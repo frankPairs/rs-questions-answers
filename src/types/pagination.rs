facf65c885d1c66bdc0d686e14d67acbd6bb76d0
Frank Parejo <francisco.parejo.lopez@gmail.com>
2022-09-11 10:15:21 +0200

    add extract pagination tests

--- src/types/pagination.rs
+++ src/types/pagination.rs
@@ -3,7 +3,7 @@ use std::collections::HashMap;
 use handle_errors::Error;
 
 /// Pagination struct which is getting extract from query params,
-#[derive(Default, Debug)]
+#[derive(Default, Debug, PartialEq)]
 pub struct Pagination {
     /// The index of the last item that has to be returned
     pub limit: Option<u32>,
@@ -53,4 +53,87 @@ pub fn extract_pagination(params: HashMap<String, String>) -> Result<Pagination,
         .map_err(Error::ParseError)?;
 
     Ok(Pagination { limit, offset })
+}
+
+#[cfg(test)]
+mod pagination_tests {
+    use super::{extract_pagination, Error, HashMap, Pagination};
+
+    #[test]
+    fn valid_pagination() {
+        let mut params = HashMap::new();
+
+        params.insert("limit".to_string(), String::from("1"));
+        params.insert("offset".to_string(), String::from("1"));
+
+        let result = extract_pagination(params);
+
+        assert_eq!(
+            result.unwrap(),
+            Pagination {
+                limit: Some(1),
+                offset: 1
+            }
+        );
+    }
+
+    #[test]
+    fn missing_offset_parameter() {
+        let mut params = HashMap::new();
+
+        params.insert("limit".to_string(), String::from("1"));
+
+        let result = extract_pagination(params);
+
+        assert_eq!(
+            format!("{}", result.unwrap_err()),
+            format!("{}", Error::MissingParameters)
+        );
+    }
+
+    #[test]
+    fn missing_limit_parameter() {
+        let mut params = HashMap::new();
+
+        params.insert("offset".to_string(), String::from("1"));
+
+        let result = extract_pagination(params);
+
+        assert_eq!(
+            format!("{}", result.unwrap_err()),
+            format!("{}", Error::MissingParameters)
+        );
+    }
+
+    #[test]
+    fn parse_limit_parameter_error() {
+        let mut params = HashMap::new();
+
+        params.insert("limit".to_string(), String::from("a"));
+        params.insert("offset".to_string(), String::from("5"));
+
+        let result = extract_pagination(params);
+        let parse_int_error = "a".parse::<u32>();
+
+        assert_eq!(
+            format!("{}", result.unwrap_err()),
+            format!("{}", Error::ParseError(parse_int_error.unwrap_err()))
+        );
+    }
+
+    #[test]
+    fn parse_offset_parameter_error() {
+        let mut params = HashMap::new();
+
+        params.insert("limit".to_string(), String::from("5"));
+        params.insert("offset".to_string(), String::from("a"));
+
+        let result = extract_pagination(params);
+        let parse_int_error = "a".parse::<u32>();
+
+        assert_eq!(
+            format!("{}", result.unwrap_err()),
+            format!("{}", Error::ParseError(parse_int_error.unwrap_err()))
+        );
+    }
 }
